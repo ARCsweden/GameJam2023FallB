b0ae3baba57cee8ed02f1299245b69fdaac94809
Per Lenander <per.lenander@addiva.se>
2023-09-30 12:09:38 +0200

    Intro dialog

--- Scripts/State.gd
+++ Scripts/State.gd
@@ -1,4 +1,19 @@
 extends Node
 
 # Put global gamestate here
+var italian_ally = 0
 
+enum Weapons {
+	Nothing,
+	Sandwich,
+	Pen,
+}
+
+var weapon: Weapons = Weapons.Nothing
+
+enum EntryPoint {
+	Front,
+	Back,
+}
+
+var entry_point
