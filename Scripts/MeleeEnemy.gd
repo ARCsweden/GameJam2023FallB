ea87335c82ee6866191d988fd1d2fa0f3a53086d
Seasnakez <adde456789@gmail.com>
2023-09-30 17:24:31 +0200

    Added delay between attacks

--- Scripts/MeleeEnemy.gd
+++ Scripts/MeleeEnemy.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
 
 var SPEED = 200
+var able_to_attack = true
 
 func _physics_process(delta):
 	# MeleeEnemy constantly moves towards the Player and attempts to attack them
 	var Player = get_node("../../CharacterBody2D")
 	var direction = Player.position - position
 	velocity = direction.normalized() * SPEED
+	
 	if direction.length() < 200:  # Stops if near player and attacks
 		velocity = Vector2(0, 0)
-		_attack()
+		if able_to_attack:
+			able_to_attack = false
+			$Timer.start()
+			_attack()
+		
 	move_and_slide()
 
 func _attack():
 	var Player = get_node("../../CharacterBody2D")
 	Player.damaged()
+
+func _on_timer_timeout():
+	able_to_attack = true
