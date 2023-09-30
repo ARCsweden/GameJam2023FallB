909dccbdcb666aeaf30bea56d4ee37d00cdd1583
Seasnakez <adde456789@gmail.com>
2023-09-30 17:08:13 +0200

    Player can now take damage and die

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 signal throw_item(player_pos)
 
 const SPEED = 1000.0
+var max_health = 3
+var current_health = 0
 
 func ready():
-	print("hello")
-	
-
+	current_health = max_health
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -15,11 +15,16 @@ func get_input():
 	# if weapons has children:
 	if Input.is_action_pressed("ui_up"):
 		emit_signal("throw_item", position)
-		
-		
 
 func _physics_process(delta):
 	get_input()
-
 	$Charachter/AnimationPlayer.play("idle")
 	move_and_slide()
+
+func damaged():
+	current_health -= 1
+	if current_health <= 0:
+		kill()
+
+func kill():
+	get_tree().reload_current_scene()
