f52a9639957fd091621907fe3258c9fe4d855628
Seasnakez <adde456789@gmail.com>
2023-09-30 21:35:08 +0200

    Fixed bug of text not dissapearing when picking up weapon

--- Scripts/WeaponManager.gd
+++ Scripts/WeaponManager.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var picked_up = false
 var pickupable= false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,16 +9,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if pickupable and (Input.is_action_just_pressed("pickup")):
+	if pickupable and (Input.is_action_just_pressed("pickup")) and not picked_up:
 		$WeaponSprite2D.visible=false
 		$interact.visible=false 
+		picked_up = true
 
 
 func _on_weapon_pickup_area_2d_body_entered(body):
-	$interact.visible=true
-	pickupable= true
-	
-
+	if not picked_up:
+		$interact.visible=true
+		pickupable= true
 
 func _on_weapon_pickup_area_2d_body_exited(body):
 	$interact.visible=false 
