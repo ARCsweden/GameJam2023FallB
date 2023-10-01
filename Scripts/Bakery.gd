01538cca8a9d01c34becac8305238a5b6ec89b36
Per Lenander <per.lenander@addiva.se>
2023-10-01 13:17:15 +0200

    fix

--- Scripts/Bakery.gd
+++ Scripts/Bakery.gd
@@ -28,5 +28,5 @@ func _on_dialogue_ended(_resource: DialogueResource):
 	enemy3.SPEED=200
 	player.able_to_attack=true
 
-func _exit_area_entered(body):
+func _exit_area_entered(_body):
 	get_tree().change_scene_to_file("res://Scenes/Kitchen.tscn")
