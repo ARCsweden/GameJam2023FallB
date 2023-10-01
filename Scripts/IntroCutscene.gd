c90e9414a57caca19a73fad1a0de04abeb79caa6
Per Lenander <per.lenander@addiva.se>
2023-10-01 11:09:29 +0200

    Intro done.

--- Scripts/IntroCutscene.gd
+++ Scripts/IntroCutscene.gd
@@ -1,10 +1,11 @@
 extends Control
 
 @onready var fade = $Fade
-@onready var image = $image
+@onready var image = $Image
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	State.set_bg_image.connect(set_bg_image)
 	var tween = get_tree().create_tween()
 	tween.tween_property(fade, "modulate", Color(1, 1, 1, 0), 1)
 	tween.tween_callback(start_dialog)
@@ -20,3 +21,6 @@ func _on_dialogue_ended(_resource: DialogueResource):
 	
 func goto_bakery():
 	get_tree().change_scene_to_file("res://Scenes/Bakery.tscn")
+
+func set_bg_image(texture: Texture2D):
+	image.set_texture(texture)
--- Scripts/State.gd
+++ Scripts/State.gd
@@ -27,3 +27,17 @@ var killed_jean_pierre = false
 var joined_carabella = false
 var ran_away = false
 
+
+# Textures, ugly
+var tex_airplane = preload("res://Asset/BGs/airplane.png")
+var tex_alley = preload("res://Asset/BGs/alley.png")
+var tex_bakery = preload("res://Asset/BGs/bakery.png")
+var tex_carabella = preload("res://Asset/BGs/carabella.png")
+var tex_don = preload("res://Asset/BGs/don.png")
+var tex_driving = preload("res://Asset/BGs/driving.png")
+var tex_hitman = preload("res://Asset/BGs/hitman.png")
+var tex_mafia = preload("res://Asset/BGs/mafia.png")
+var tex_mansion = preload("res://Asset/BGs/mansion.png")
+var tex_monaco = preload("res://Asset/BGs/monaco.png")
+
+signal set_bg_image(texture: Texture2D)
